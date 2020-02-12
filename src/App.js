d7218965dc695da9185ff5c3a05c43c5c456e985
hyeinjang <janghyein0@gmail.com>
2020-02-12 18:40:55 +0900

    cameo practice 2 starts from here

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <h3>hello world!</h3>
+    )
+  }
+}
+
+export default App;
